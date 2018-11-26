18eaccfca52e6edfa16f5dfae35ade5961c58c2d
Mathilde Couraud <mcouraud@outlook.fr>
2018-11-26 16:29:02 +0100

    Validates presence of in Models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+  validates :start_date, presence: :true
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,5 @@
 class Flat < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :city, :address, :available_from, :flat_surface, :room_surface, :number_of_rooms, :number_of_roommates, :rent, :user_id, presence: :true
 end
