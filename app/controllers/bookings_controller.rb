7494e762ae9108652daae07bee9bd15bfe026099
Kardali Bechar <bechar.kardali@laposte.net>
2018-11-27 10:49:57 +0100

    bblabla

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,12 @@
 class BookingsController < ApplicationController
   def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :flat_id)
   end
 end
