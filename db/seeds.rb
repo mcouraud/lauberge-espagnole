d11ae2cc6b314244ce8fffae7206af978040d9b7
Mathilde Couraud <mcouraud@outlook.fr>
2018-11-28 17:00:47 +0100

    Changement de logo + ajout carroussel et visit dans Flat#show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'pry-byebug'
 
-user = User.new(firstname: "Mathilde", lastname: "Couraud", age: 24, email: "mcouraud@outlook.fr", nationality: "French", situation: "Le wagon student")
-user.save
+user1 = User.new(firstname: "Camille", lastname: "Menard", age: 24, email: "c.menard@outlook.fr", password: "Hello2", nationality: "Française", situation: "Le wagon student")
+#binding.pry
+user1.save!
 
-5.times do
- flat = Flat.new(city: "Marseille", address: "21 rue berlioz", available_from: Date.new(2019,01,01), flat_surface: 80, room_surface: 15, number_of_rooms: 3, number_of_roommates: 3, rent: 415, user_id: 1)
- flat.save
+user2 = User.new(firstname: "Antoine", lastname: "Pontier", age: 24, email: "a.pontier@free.fr", password: "Hello2", nationality: "Italien", situation: "Chef de projet")
+user2.save!
+
+100.times do
+  city = ["Marseille", "Lille", "Bordeaux", "Paris", "Lyon", "Nantes", "Toulouse"].sample
+  address = ["14 rue Charles de Gaulle", "22 rue Louis Pasteur", "8 rue Victor Hugo", "31 rue Jean Jaurès", "83 boulevard Léon Gambetta"].sample
+  flat_surface = [70, 80, 90, 95, 100, 110, 120, 130].sample
+  rent = [300, 320, 340, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450].sample
+  number = [2, 3, 4, 5].sample
+
+ flat = Flat.new(city: city, address: address, available_from: Date.new(2019,01,01), flat_surface: flat_surface, room_surface: 12, number_of_rooms: number, number_of_roommates: number, rent: rent, user: [User.first, User.last].sample)
+
+ flat.save!
 end
 
